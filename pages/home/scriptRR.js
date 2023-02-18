c1ef070190f4678cb9e0b96e2c3e06f8855e5587
JoãoVitor <99548321+joavitormartins@users.noreply.github.com>
2023-02-18 12:57:51 -0300

    Add files via upload

---
+++ pages/home/scriptRR.js
@@ -0,0 +1,32 @@
+const login = document.querySelector('.clear')
+
+login.addEventListener('click',voltar);
+login.addEventListener('touch',voltar);
+
+
+function voltar(){
+    window.location.href="/index.html"
+}
+
+function register(){
+const senhaUM = document.querySelector("#password").value
+const senhaDOIS = document.querySelector("#confirmPassword").value
+const faltoudigito = document.querySelector("#password-min-length-error")
+const senhasdiferentes = document.querySelector("#password-doesnt-match-error")
+
+
+if (senhaUM.length < 6) {
+faltoudigito.style.display = "block"
+    
+}
+else if
+(senhaUM == senhaDOIS){
+
+    
+}
+else
+{
+    console.log("não foi")
+    senhasdiferentes.style.display = "block"
+}
+}
